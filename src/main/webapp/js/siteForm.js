26c58b62b8e42d041f268d34b8b80ad6bc01f658
ptemkar <enzo.660@gmail.com>
2012-09-18 00:30:25 -0700

    coeditor config changes. hiding other divs when coeditor is opened

--- src/main/webapp/js/siteForm.js
+++ src/main/webapp/js/siteForm.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
 	
 	$("#showEditorLink").click(function(){
+		$('#topNavigation').toggle();
+		$('#footer').toggle();	
 		$('#detailsDiv').toggle();
 		$('#contentDiv').toggle();
 		$("#showEditorLink").toggle();
 		$("#closeEditorLink").toggle();
-		//$('#siteFormMain').removeClass("genericForm");
-		CKEDITOR.replace( 'content' );
+		$('#siteFormMain').removeClass("genericForm");
+		CKEDITOR.replace( 'content', {
+
+		});
+		//$('#cke_contents_content').addClass("ckEditorHeight");
 	});
 	
 	$("#closeEditorLink").click(function(){
+		$('#topNavigation').toggle();
+		$('#footer').toggle();
 		$('#detailsDiv').toggle();
 		$('#contentDiv').toggle();
 		$("#showEditorLink").toggle();
 		$("#closeEditorLink").toggle();
+		$('#siteFormMain').addClass("genericForm");
 	});
 	
  });
